3354082dc88ad630ff1809ccc03d48b25c7526ed
dchilot <dchilot@hotmail.com>
2013-06-15 19:09:16 +0200

    Make it possible to install virtualenv system wide.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,13 @@ mkdir -p $HOME/env
 if [ -z "$TMPDIR" ] ; then
 	export TMPDIR=$TMP
 fi
-cd $TMPDIR
 if [ -z "$(which virtualenv)" ] ; then
+	cd $TMPDIR
 	wget -nc --no-check-certificate https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.1.tar.gz
 	cd $TMPDIR
 	tar xzf virtualenv-1.9.1.tar.gz
 	cd virtualenv-1.9.1
-	python setup.py install
+	python setup.py install || sudo python setup.py install
 fi
 cd $dir
 pwd
